032add7d08a1d3d83a5cb8caac2cfa36afdac35c
Wirkus Tomasz <wirkus919@gmail.com>
2020-06-14 13:28:32 +0200

    Attempted to remove bugg with not loading images due to CORS policy in html2canvas pdf doc

--- client/src/components/user/resumePreview/Photo.jsx
+++ client/src/components/user/resumePreview/Photo.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import me from '../../../img/users/me.png';
 
 const Photo = ({ user }) => {
   if (!user) user = require('../../../utils/defaultUser.json');
 
   return (
     <div className="resumePreview__photo">
-      <img src={me} alt="user" className="resumePreview__photo__img" />
+      <img src={user.photo} alt="user" className="resumePreview__photo__img" />
     </div>
   );
 };
